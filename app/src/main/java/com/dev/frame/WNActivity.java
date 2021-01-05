25b50591edb3407540aa34c42994860b42c558ef
wangning <1234qwer>
2021-01-05 10:52:22 +0800

    修改入口界面名称

--- app/src/main/java/com/dev/frame/WNActivity.java
+++ app/src/main/java/com/dev/frame/WNActivity.java
@@ -3,7 +3,7 @@ package com.dev.frame;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class WNActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
